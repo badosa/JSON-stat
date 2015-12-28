fd34b1963a52cc08478a177e0db14e9b3428dc99
Xavier Badosa <xavier@MacBook-Pro.local>
2015-12-28 18:54:02 +0100

    v. 0.9.7
    Not a real version: identical to 0.9.6

--- module.js
+++ module.js
@@ -1,6 +1,6 @@
 /*
 
-JSON-stat Javascript Toolkit v. 0.9.6 (JSON-stat v. 2.0 ready) (Nodejs module)
+JSON-stat Javascript Toolkit v. 0.9.7 (JSON-stat v. 2.0 ready) (Nodejs module)
 http://json-stat.com
 https://github.com/badosa/JSON-stat
 
@@ -22,7 +22,7 @@ permissions and limitations under the License.
 
 var JSONstat = JSONstat || {};
 
-JSONstat.version="0.9.6";
+JSONstat.version="0.9.7";
 
 /* jshint newcap:false */
 function JSONstat(resp,f){
