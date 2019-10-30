e649b7702d9d1df6f7f334c9d7d876ddd37de45c
Xavier Badosa <xbadosa@gmail.com>
2019-10-30 16:44:38 +0100

    v. 2.5.4

--- utils/module.max.js
+++ utils/module.max.js
@@ -1,6 +1,6 @@
 /*
 
-JSON-stat Javascript Utilities Suite v. 2.5.3 (requires JJT 0.10+) (Nodejs module)
+JSON-stat Javascript Utilities Suite v. 2.5.4 (requires JJT 0.10+) (Nodejs module)
 https://json-stat.com
 https://github.com/badosa/JSON-stat/tree/master/utils
 
@@ -1098,7 +1098,7 @@ var
 			toCSV: toCSV,
 			join: join,
 			fromSDMX: fromSDMX,
-			version: "2.5.3"
+			version: "2.5.4"
 		};
 	}()
 ;
